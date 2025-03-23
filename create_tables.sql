04dc3df176ae159a67510b87c5a44d861ea17aed
slinki17 <127741672+slinki17@users.noreply.github.com>
2025-03-23 19:52:24 +0100

    Add files via upload

---
+++ create_tables.sql
@@ -0,0 +1,53 @@
+create table Users(
+
+	user_id int primary key,
+	name varchar(255),
+	email varchar(255),
+	phone varchar(255),
+	type varchar(255)
+
+);
+
+create table Books(
+
+	book_id int primary key,
+	title varchar(255),
+	author varchar(255),
+	year int,
+	genre varchar(255)
+	
+);
+
+create table Copies(
+
+	copy_id int primary key,
+	book_id int,
+	status varchar(255),
+	foreign key (book_id) references Books(book_id)
+)
+
+create table Borrowing(
+
+	borrow_id int primary key,
+	user_id int,
+	copy_id int,
+	date date,
+	due date,
+	return_date date,
+	foreign key (user_id) references Users(user_id),
+	foreign key (copy_id) references Copies(copy_id)
+
+)
+
+create table Reservations(
+
+	reservation_id int primary key,
+	user_id int,
+	book_id int,
+	reservation_date date,
+	expiration_date date,
+	status varchar(255),
+	foreign key (user_id) references Users(user_id),
+	foreign key (book_id) references Books(book_id)
+
+)
